ad39350d7cdd23e627ce32d86d862d295b40c06e
andreanovix <novix84@gmail.com>
2014-09-29 15:53:30 +0200

    Cancellato cartella inc

--- public/inc/scripts.inc.php
+++
@@ -1,6 +0,0 @@
-<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-
-<script src="js/jquery.placeholder.js"></script>
-<script src="js/custom.js"></script>
-<script src="js/classie.js"></script>
